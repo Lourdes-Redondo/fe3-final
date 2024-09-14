085ed55c0ed12f4a529cc602a677467706260a94
Lourdes Redondo <lourdesred835@gmail.com>
2024-09-14 11:24:28 -0300

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,38 @@
 
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
-
+import React, { useState } from 'react'
+import { Route } from "react-router-dom";
+import './Estilos.css';
 
 function App() {
+
+  const [theme, setTheme] = useState('light-theme');
+
+  const toggleTheme = () => {
+    setTheme((prevTheme) =>
+      prevTheme === 'light-theme' ? 'dark-theme' : 'light-theme'
+    );
+  };
   return (
+    <>
       <div className="App">
           <Navbar/>
+          <Route path="/" element = {<home />} />
+          <Route path="/contact" element = {<contact />} />
+          <Route path="/dentist" element = {<dentist />} />
+          <Route path="/favs" element = {<favs />} />
+          <Route path="*" element = {<h1>Error 404 - Page not found</h1>} />
+
           <Footer/>
       </div>
+
+      <div className={theme}>
+      <Navbar theme={theme} toggleTheme={toggleTheme} />
+    </div>
+  );
+
+    </>
   );
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
-
+import { Link, useNavigate } from "react-router-dom";
+import './Estilos.css'
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
-const Navbar = () => {
+const Navbar = ({ theme, toggleTheme }) => {
+  const navigate = useNavigate();
 
   return (
-    <nav>
+    <nav clasName="navbar">
+       <button onClick={() => navigate(-1)}>🔙</button>
+      <link to = "/"> <h4>Inicio</h4> </link>
+      <link to = "/contact"> <h4>Contacto</h4> </link>
+      <link to = "/dentist"> <h4>Detalle del dentista</h4> </link>
+      <link to = "/favs"> <h4>Destacados</h4> </link>
       {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
       {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button>Change theme</button>
+      
+      <nav style={{ background: theme.background, color: theme.color }}></nav>
+
+      <button onClick={toggleTheme}>Change theme</button>
     </nav>
   )
 }
